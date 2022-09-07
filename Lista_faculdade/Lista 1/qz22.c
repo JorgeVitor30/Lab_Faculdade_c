341ac820807170866eba9611dfd97398d05b5f06
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2022-09-07 13:19:50 -0300

    Create qz22.c

---
+++ Lista_faculdade/Lista 1/qz22.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    // A = 65
+    printf("o número inteiro equivalente a 'A': %d \n", 'A');
+    // B = 66
+    printf("o número inteiro equivalente a 'B': %d \n", 'B');
+    // C = 67
+    printf("o número inteiro equivalente a 'C': %d \n", 'C');
+    // a = 97
+    printf("o número inteiro equivalente a 'a': %d \n", 'a');
+    // b = 98
+    printf("o número inteiro equivalente a 'b': %d \n", 'b');
+    // c = 99
+    printf("o número inteiro equivalente a 'c': %d \n", 'c');
+    // '2' = 48
+    printf("o número inteiro equivalente a '0': %d \n", '0');
+    // '1' = 49
+    printf("o número inteiro equivalente a '1': %d \n", '1');
+    // '2' = 50
+    printf("o número inteiro equivalente a '2': %d \n", '2');
+    // $ = 36
+    printf("o número inteiro equivalente a '$': %d \n", '$');
+    // * = 42
+    printf("o número inteiro equivalente a '*': %d \n", '*');
+    // + = 43
+    printf("o número inteiro equivalente a '+': %d \n", '+');
+    // / = 47
+    printf("o número inteiro equivalente a '/': %d \n", '/');
+    // SPACE = 32
+    printf("o número inteiro equivalente a 'SPACE': %d \n", ' ');
+    return 0;
