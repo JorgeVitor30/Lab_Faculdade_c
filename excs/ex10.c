410e426672d17e0236cd029b713debdc799fdc3d
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2022-11-13 20:24:06 -0300

    Create ex10.c

---
+++ excs/ex10.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TAM 10
+
+void preencherVetor(int *, int, int);
+void printaVetor(int *, int);
+
+int main(){
+    srand(time(NULL));
+
+    int *v;
+    v = malloc(TAM * sizeof(int));
+    
+    preencherVetor(v, 0, TAM);
+    printaVetor(v, TAM);
+    
+    int qtd;
+    puts("Digite o novo tamanho: ");
+    scanf("%d",&qtd);
+    if (qtd < TAM){
+        puts("Tamanho inválido");
+        exit(1);
+    }
+    
+    v = realloc(v, qtd - TAM);
+    preencherVetor(v, TAM, qtd - TAM);
+    printaVetor(v, TAM + (qtd - TAM));
+    
+    printf("%d",TAM + qtd);
+    
+
+    return 0;
+}
+
+
+void preencherVetor
+(int *v, int inicio , int fim){
+    for(int i = 0; i < fim; i++){
+        *(v + i) = rand() % 100;
+    }
+}
+
+void printaVetor(int *v, int tamanho){
+    for(int i = 0; i < tamanho; i++){
+        printf("%d -> %p\n",*(v + i), v + i);
+    }
+}
+
